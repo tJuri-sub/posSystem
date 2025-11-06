06803981f4f5664489e8b66665e88f16472706c4
tjuri-sub <juricuber@gmail.com>
2025-11-06 22:42:36 +0800

    modified alot in stylings for inventory

--- components/styles/inventory.ts
+++ components/styles/inventory.ts
@@ -4,75 +4,75 @@ export default StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    paddingBottom: 150,
+    paddingBottom: 120,
     paddingHorizontal: 10,
 
     backgroundColor: '#FBFBFB',
   },
 
-  Title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 7,
+  tableEntryContainer: {
+    marginVertical: 10,
+    height: '100%',
   },
 
-  tableHeader: {
-    flexDirection: 'row',
+  idHeader: {
+    marginBottom: 5,
+    paddingBottom: 5,
 
-    backgroundColor: '#999',
-    borderTopLeftRadius: 3,
-    borderTopRightRadius: 3,
-
-    marginTop: 10,
-    paddingHorizontal: 5,
+    borderBottomWidth: 1,
+    borderColor: 'hsla(100, 18%, 13%, .2)',
   },
 
-  tableText: {
-    fontSize: 16,
-  },
-
-  tableFlex: {
-    padding: 5,
-  },
-
-  tableWidth1: {
-    width: '60%',
-  },
-
-  tableWidth2: {
-    width: '20%',
-  },
-
-  tableWidth3: {
-    width: '20%',
-  },
-
-  tableEntryButton: {
-    marginTop: 5,
-    marginHorizontal: 5,
+  entryContainer: {
+    paddingHorizontal: 15,
     paddingVertical: 10,
+    marginBottom: 19,
 
     backgroundColor: '#fff',
-    borderRadius: 4,
+
+    borderRadius: 7,
+    borderWidth: 1,
+    borderColor: 'hsla(100, 18%, 13%, .1)',
+
+    boxShadow: '2px 2px 3px hsla(0, 0%, 0%, .2)',
   },
 
-  tableEntryContainer: {
-    height: '100%',
-    overflow: 'hidden',
+  flexContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 
-    backgroundColor: '#c8c8c8ff',
-    borderBottomLeftRadius: 3,
-    borderBottomRightRadius: 3,
+  actionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
 
-    boxShadow:
-      'inset 2px 2px 3px rgba(0,0,0,0.2), inset -2px -2px 3px rgba(255, 255, 255, 0.2),',
+  productDescriptionContainer: {
+    flexDirection: 'row',
+    gap: 20,
+  },
+
+  productNameContainer: {
+    marginVertical: 10,
+  },
+
+  productPrizeContainer: {
+    flexDirection: 'row',
+  },
+
+  productQuantityContainer: {
+    flexDirection: 'row',
   },
 
   tableEntryName: {
+    fontWeight: 'bold',
     fontSize: 20,
   },
 
   tableEntryText: {
+    fontWeight: 'bold',
     fontSize: 16,
   },
 
@@ -80,9 +80,7 @@ export default StyleSheet.create({
     textAlign: 'center',
   },
 
-  tableHeaderEntry: {
-    flexDirection: 'row',
-  },
+  Entry: {},
 
   addButton: {
     display: 'flex',
@@ -93,7 +91,7 @@ export default StyleSheet.create({
     width: '17%',
     aspectRatio: 1 / 1,
 
-    backgroundColor: '#B79600',
+    backgroundColor: '#00487C',
     borderRadius: '50%',
     boxShadow: '0px 2px 3px rgba(0,0,0,0.3)',
 
@@ -101,6 +99,7 @@ export default StyleSheet.create({
     bottom: 20,
     right: 20,
   },
+
   flexAlign: {
     flex: 1,
     display: 'flex',
